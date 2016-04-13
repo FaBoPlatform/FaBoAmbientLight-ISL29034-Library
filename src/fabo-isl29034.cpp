9e7061da8029e129f0cd13643e135d723dc15bc4
Shimakage <shimakage@gclue.jp>
2016-04-13 09:05:03 +0900

    add comment

--- src/fabo-isl29034.cpp
+++ src/fabo-isl29034.cpp
@@ -1,15 +1,16 @@
+/**
+ * @file  fabo-isl29034.cpp
+ * @brief fabo libtary of ISL29034
+ * @author Akira Sasaki
+ * @date 2,10, 2016
+ */
+
 #include "fabo-isl29034.h"
 
-void isl29034::powerOn()
-{
-  writeI2c(ISL29034_COMMAIN_I_REG, ISL29034_ALS_CONTINUOSULY);  
-}
-
-void isl29034::configuration()
-{
-  writeI2c(ISL29034_COMMAIN_II_REG, ISL29034_LUX_RANGE_64000|ISL29034_CLOCK_65536);  
-}
-
+/**
+ * @brief Read Sensor Data
+ * @return uint16_t : Read Data
+ */
 uint16_t isl29034::readData()
 {
   uint16_t ambient;
@@ -21,32 +22,55 @@ uint16_t isl29034::readData()
   return ambient;
 }
 
-// I2Cへの書き込み
-void isl29034::writeI2c(byte register_addr, byte value) {
-  Wire.beginTransmission(HTS221_SLAVE_ADDRESS);  
-  Wire.write(register_addr);         
-  Wire.write(value);                 
-  Wire.endTransmission();        
+/**
+ * @brief Set Config
+ */
+void isl29034::configuration()
+{
+  writeI2c(ISL29034_COMMAND_II_REG, ISL29034_LUX_RANGE_64000|ISL29034_CLOCK_65536);
 }
 
-// I2Cへの読み込み
-void isl29034::readI2c(byte register_addr, int num, byte *buf) {
-  Wire.beginTransmission(HTS221_SLAVE_ADDRESS); 
-  Wire.write(register_addr);           
-  Wire.endTransmission(false);         
+/**
+ * @brief Power ON
+ */
+void isl29034::powerOn()
+{
+  writeI2c(ISL29034_COMMAND_I_REG, ISL29034_ALS_CONTINUOSULY);
+}
 
-  //Wire.beginTransmission(DEVICE_ADDR); 
-  Wire.requestFrom(HTS221_SLAVE_ADDRESS, num);  
+/**
+ * @brief Write I2C Data
+ * @param [in] register_addr : Write Register Address
+ * @param [in] value  : Write Data
+ */
+void isl29034::writeI2c(byte register_addr, byte value) {
+  Wire.beginTransmission(HTS221_SLAVE_ADDRESS);
+  Wire.write(register_addr);
+  Wire.write(value);
+  Wire.endTransmission();
+}
+
+/**
+ * @brief Read I2C Data
+ * @param [in] register_addr : register address
+ * @param [in] num   : Data Length
+ * @param [out] *buf : Read Data
+ */
+void isl29034::readI2c(byte register_addr, int num, byte *buf) {
+  Wire.beginTransmission(HTS221_SLAVE_ADDRESS);
+  Wire.write(register_addr);
+  Wire.endTransmission(false);
+
+  //Wire.beginTransmission(DEVICE_ADDR);
+  Wire.requestFrom(HTS221_SLAVE_ADDRESS, num);
 
   int i = 0;
   while (Wire.available())
   {
-    buf[i] = Wire.read(); 
-    i++;   
+    buf[i] = Wire.read();
+    i++;
   }
-  //Wire.endTransmission();         
+  //Wire.endTransmission();
 }
 
-
 isl29034 faboAmbientLight;
-
